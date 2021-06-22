850f5d511ec792d993818c7f188652ff92ca054c
Jade White <jade@intergalactic.com>
2021-06-22 13:43:15 -0600

    Formatting

--- example/App.js
+++ example/App.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,8 +14,8 @@ import {
   View,
   Text,
   StatusBar,
-    Button,
-    Alert
+  Button,
+  Alert,
 } from 'react-native';
 
 import {
@@ -25,78 +25,80 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import UnityView, { UnityModule } from '@asmadsen/react-native-unity-view';
+import UnityView, {UnityModule} from '@asmadsen/react-native-unity-view';
 
 const App: () => React$Node = () => {
-  const [count, setClickCount] = useState(0)
-  console.log(count)
+  const [count, setClickCount] = useState(0);
+  console.log(count);
   const onUnityMessage = (hander) => {
-    console.log({hander})
-  }
+    console.log({hander});
+  };
 
   const onClick = () => {
-      UnityModule.postMessageToUnityManager({
-                                                name: 'ToggleRotate',
-                                                data: '',
-                                                callBack: (data) => {
-                                                    Alert.alert('Tip', JSON.stringify(data))
-                                                }
-                                            });
-  }
-
+    UnityModule.postMessageToUnityManager({
+      name: 'ToggleRotate',
+      data: '',
+      callBack: (data) => {
+        Alert.alert('Tip', JSON.stringify(data));
+      },
+    });
+  };
 
   return (
-      <View  style={{ flex: 1 }}>
-          <View style={{ flex: 1 }}>
-            <UnityView
-                style={{ flex: 1 }}
-                onMessage={onUnityMessage}
-                onUnityMessage={onUnityMessage}
-            />
-          </View>
-        <Button style={{ width: '100%' }} title="Toggle rotation" onPress={onClick}/>
-
+    <View style={{flex: 1}}>
+      <View style={{flex: 1}}>
+        <UnityView
+          style={{flex: 1}}
+          onMessage={onUnityMessage}
+          onUnityMessage={onUnityMessage}
+        />
       </View>
+      <Button
+        style={{width: '100%'}}
+        title="Toggle rotation"
+        onPress={onClick}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-                                   scrollView: {
-                                     backgroundColor: Colors.lighter,
-                                   },
-                                   engine: {
-                                     position: 'absolute',
-                                     right: 0,
-                                   },
-                                   body: {
-                                     backgroundColor: Colors.white,
-                                   },
-                                   sectionContainer: {
-                                     marginTop: 32,
-                                     paddingHorizontal: 24,
-                                   },
-                                   sectionTitle: {
-                                     fontSize: 24,
-                                     fontWeight: '600',
-                                     color: Colors.black,
-                                   },
-                                   sectionDescription: {
-                                     marginTop: 8,
-                                     fontSize: 18,
-                                     fontWeight: '400',
-                                     color: Colors.dark,
-                                   },
-                                   highlight: {
-                                     fontWeight: '700',
-                                   },
-                                   footer: {
-                                     color: Colors.dark,
-                                     fontSize: 12,
-                                     fontWeight: '600',
-                                     padding: 4,
-                                     paddingRight: 12,
-                                     textAlign: 'right',
-                                   },
-                                 });
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
 
 export default App;
--- src/UnityView.tsx
+++ src/UnityView.tsx
@@ -1,84 +1,79 @@
-import * as React from 'react'
-import { NativeModules, requireNativeComponent, View, ViewProps, ViewPropTypes } from 'react-native'
-import * as PropTypes from 'prop-types'
-import MessageHandler from './MessageHandler'
-import { UnityModule, UnityViewMessage } from './UnityModule'
-import { Component, useEffect, useState } from 'react'
-
-const { UIManager } = NativeModules
+import * as React from 'react';
+import { requireNativeComponent, View, ViewProps } from 'react-native';
+import MessageHandler from './MessageHandler';
+import { UnityModule } from './UnityModule';
+import { Component } from 'react';
 
 export interface UnityViewProps extends ViewProps {
-    /**
-     * Receive string message from unity.
-     */
-    onMessage?: (message: string) => void;
-    /**
-     * Receive unity message from unity.
-     */
-    onUnityMessage?: (handler: MessageHandler) => void;
+  /**
+   * Receive string message from unity.
+   */
+  onMessage?: (message: string) => void;
+  /**
+   * Receive unity message from unity.
+   */
+  onUnityMessage?: (handler: MessageHandler) => void;
 
-    children?: React.ReactNode
+  children?: React.ReactNode;
 
-    /**
-     * Calls Application.Unload() when the view is unmounted.
-     */
-    unloadOnUnmount?: boolean;
+  /**
+   * Calls Application.Unload() when the view is unmounted.
+   */
+  unloadOnUnmount?: boolean;
 }
 
-let NativeUnityView
+let NativeUnityView;
 
 class UnityView extends Component<UnityViewProps> {
+  state = {
+    handle: null,
+  };
 
-    state = {
-        handle: null
-    }
+  componentDidMount(): void {
+    const { onUnityMessage, onMessage, unloadOnUnmount } = this.props;
+    console.log('Mounting the thing');
 
-    componentDidMount(): void {
-        const { onUnityMessage, onMessage, unloadOnUnmount } = this.props
-        console.log("Mounting the thing")
-
-        this.setState({
-            handle: UnityModule.addMessageListener(message => {
-                if (onUnityMessage && message instanceof MessageHandler) {
-                    onUnityMessage(message)
-                }
-                if (onMessage && typeof message === 'string') {
-                    onMessage(message)
-                }
-            })
-        })
-        if (unloadOnUnmount) {
-            UnityModule.reloadAfterUnload();
+    this.setState({
+      handle: UnityModule.addMessageListener(message => {
+        if (onUnityMessage && message instanceof MessageHandler) {
+          onUnityMessage(message);
         }
-    }
-
-    componentWillUnmount(): void {
-        console.log("Unmounting The thing")
-        const { unloadOnUnmount } = this.props;
-
-        UnityModule.removeMessageListener(this.state.handle)
-
-        if (unloadOnUnmount) {
-            UnityModule.unload();
+        if (onMessage && typeof message === 'string') {
+          onMessage(message);
         }
+      }),
+    });
+    if (unloadOnUnmount) {
+      UnityModule.reloadAfterUnload();
     }
+  }
 
-    render() {
-        const { props } = this
-        return (
-            <View {...props}>
-            <NativeUnityView
-                style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }}
-                onUnityMessage={props.onUnityMessage}
-                onMessage={props.onMessage}
-            >
-            </NativeUnityView>
-            {props.children}
-        </View>
-        )
+  componentWillUnmount(): void {
+    console.log('Unmounting The thing');
+    const { unloadOnUnmount } = this.props;
+
+    UnityModule.removeMessageListener(this.state.handle);
+
+    if (unloadOnUnmount) {
+      UnityModule.unload();
     }
+  }
+
+  render() {
+    const { props } = this;
+    return (
+      <View {...props}>
+        <NativeUnityView
+          style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }}
+          onUnityMessage={props.onUnityMessage}
+          onMessage={props.onMessage}
+        />
+        {props.children}
+      </View>
+    );
+  }
 }
 
-NativeUnityView = requireNativeComponent('RNUnityView', UnityView)
+NativeUnityView = requireNativeComponent('RNUnityView', UnityView);
 
 export default UnityView;
